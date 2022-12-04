374dbf49279394b7cfc87148ab0ea97aa0d2b18b
Riley Shopmyer <rileyshopmyer@gmail.com>
2022-12-04 17:32:01 -0500

    A03 Attempt 2

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,10 @@
 // File that runs when you link/install pkg and run roll-dice
 
 import minimist from 'minimist';
-import { roll } from "/lib/roll.js"
+import { roll } from "../lib/roll.js"
 
 const argv = minimist(process.argv.slice(2));
 
-console.log(JSON.stringify(argv));
+const result = roll(argv.sides, argv.dice, argv.rolls);
+
+console.log(JSON.stringify(result));
